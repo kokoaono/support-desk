b807cc94102b87d82ec2bb0b8543bf6295de52d4
kokoaono <aono@Ayakos-MacBook-Pro.local>
2022-07-21 16:15:45 +1200

    add routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+//Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});

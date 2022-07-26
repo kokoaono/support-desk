ffc7228cc65b81c405a8982996702ced628b2bc2
kokoaono <aono@Ayakos-MacBook-Pro.local>
2022-07-26 22:40:35 +1200

    connect db

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,25 @@
-const registerUser = (req, res) => {
-  res.send("Register Route");
-};
+const asyncHandler = require("express-async-handler");
 
-const loginUser = (req, res) => {
+//register a new user
+//route /api/users
+//@access public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  //validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
   res.send("Register Route");
-};
+});
+
+//Login a new user
+//route /api/login
+//@access public
+
+const loginUser = asyncHandler(async (req, res) => {
+  res.send("Register Route");
+});
 
 module.exports = {
   registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
+//connect to DB
+connectDB();
+
 const app = express();
+
+//get data from the body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello world");
@@ -10,6 +20,8 @@ app.get("/", (req, res) => {
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);

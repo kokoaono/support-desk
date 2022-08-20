83fe7db820f6f78c6968b9aee3de3e2e089153b8
kokoaono <aono@Ayakos-MacBook-Pro.local>
2022-08-21 11:15:09 +1200

    add tickets component

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -13,8 +13,20 @@ const createTicket = async (ticketData, token) => {
   return res.data;
 };
 
+//get user tickets
+const getTickets = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const res = await axios.get(API_URL, config);
+  return res.data;
+};
+
 const ticketService = {
   createTicket,
+  getTickets,
 };
 
 export default ticketService;

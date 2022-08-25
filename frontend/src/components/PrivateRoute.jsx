b912b2e9767df778684daa779b170830463b4703
kokoaono <aono@Ayakos-MacBook-Pro.local>
2022-08-26 09:55:00 +1200

    remove useAuth hook

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,14 +1,12 @@
+import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuthStatus } from "../hooks/useAuthStatus";
-import Spinner from "./Spinner";
 
-const PrivateRoute = () => {
-  const { loggedIn, checkingStatus } = useAuthStatus();
-  if (checkingStatus) {
-    return <Spinner />;
-  }
 
-  return loggedIn ? <Outlet /> : <Navigate to="/login" />;
+const PrivateRoute = ({ children }) => {
+  const { user } = useSelector((state) => state.auth);
+  if (user) return children;
+
+  return <Navigate to="/login" />;
 };
 
 export default PrivateRoute;

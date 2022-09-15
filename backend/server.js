1e531173981366c5285c3879f8aa1dc1c96ad6d7
kokoaono <aono@Ayakos-MacBook-Pro.local>
2022-09-15 13:03:07 +1200

    change path

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,8 @@ app.use("/api/tickets", require("./routes/ticketRoutes"));
 if (process.env.NODE_ENV === "production") {
   //set build folder as static
   app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  app.get("*", (_, res) =>
+    res.sendFile(__dirname, "../", "frontend/build/index.html")
   );
 } else {
   app.get("/", (req, res) => {

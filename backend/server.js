ccc89644a3c27f87ec7818314b7e1fafde2a812f
kokoaono <aono@Ayakos-MacBook-Pro.local>
2022-09-17 08:17:32 +1200

    remove json

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,6 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (_, res) =>
     res.sendFile(__dirname, "../", "frontend/build/index.html")
   );
-} else {
-  app.get("/", (req, res) => {
-    res.status(200).json({ message: "Welcome to the support Desk API" });
-  });
 }
 
 app.use(errorHandler);
